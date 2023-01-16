d71c690605e95db806ae989e13bb4f32230407fd
moh alqasab <mmalqsab@gmail.com>
2023-01-16 11:11:26 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GRP',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
